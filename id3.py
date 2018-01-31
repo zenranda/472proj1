94a8811e6deafccd67ad6db3bca2da5dd3e15c89
First Last <mfree8@hushmail.com>
2018-01-30 17:36:44 -0800

    Project created.

---
+++ id3.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+#
+# CIS 472/572 -- Programming Homework #1
+#
+# Starter code provided by Daniel Lowd, 1/25/2018
+#
+#
+import sys
+import re
+# Node class for the decision tree
+import node
+
+#for entropy calcs
+import math
+
+train=None
+varnames=None
+test=None
+testvarnames=None
+root=None
+
+# Helper function computes entropy of Bernoulli distribution with
+# parameter p
+def entropy(p):
+        lf = p
+        rh = 1 - p
+        res = -(rh * math.log(rh, 2)) - (lf*math.log(lf, 2))
+        return res;
+
+# Compute information gain for a particular split, given the counts
+# py_pxi : number of occurences of y=1 with x_i=1 for all i=1 to n
+# pxi : number of occurrences of x_i=1
+# py : number of ocurrences of y=1
+# total : total length of the data
+def infogain(py_pxi, pxi, py, total):
+	# >>>> YOUR CODE GOES HERE <<<<
+    # For now, always return "0":
+	return 0;
+
+# OTHER SUGGESTED HELPER FUNCTIONS:
+# - collect counts for each variable value with each class label
+# - find the best variable to split on, according to mutual information
+# - partition data based on a given variable	
+	
+	
+	
+# Load data from a file
+def read_data(filename):
+    f = open(filename, 'r')
+    p = re.compile(',')
+    data = []
+    header = f.readline().strip()
+    varnames = p.split(header)
+    namehash = {}
+    for l in f:
+        data.append([int(x) for x in p.split(l.strip())])
+    return (data, varnames)
+
+# Saves the model to a file.  Most of the work here is done in the
+# node class.  This should work as-is with no changes needed.
+def print_model(root, modelfile):
+    f = open(modelfile, 'w+')
+    root.write(f, 0)
+
+# Build tree in a top-down manner, selecting splits until we hit a
+# pure leaf or all splits look bad.
+def build_tree(data, varnames):
+    # >>>> YOUR CODE GOES HERE <<<<
+    # For now, always return a leaf predicting "1":
+    return node.Leaf(varnames, 1)
+
+
+# "varnames" is a list of names, one for each variable
+# "train" and "test" are lists of examples.
+# Each example is a list of attribute values, where the last element in
+# the list is the class value.
+def loadAndTrain(trainS,testS,modelS):
+	global train
+	global varnames
+	global test
+	global testvarnames
+	global root
+	(train, varnames) = read_data(trainS)
+	(test, testvarnames) = read_data(testS)
+	modelfile = modelS
+	
+	# build_tree is the main function you'll have to implement, along with
+    # any helper functions needed.  It should return the root node of the
+    # decision tree.
+	root = build_tree(train, varnames)
+	print_model(root, modelfile)
+	
+def runTest():
+	correct = 0
+	# The position of the class label is the last element in the list.
+	yi = len(test[0]) - 1
+	for x in test:
+		# Classification is done recursively by the node class.
+        # This should work as-is.
+		pred = root.classify(x)
+		if pred == x[yi]:
+			correct += 1
+	acc = float(correct)/len(test)
+	return acc	
+	
+	
+# Load train and test data.  Learn model.  Report accuracy.
+def main(argv):
+    if (len(argv) != 3):
+                print entropy(0.8571428571428)
+		print 'Usage: id3.py <train> <test> <model>'
+		sys.exit(2)
+    loadAndTrain(argv[0],argv[1],argv[2]) 
+                    
+    acc = runTest()             
+    print "Accuracy: ",acc                      
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
